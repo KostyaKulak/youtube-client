4b5be7597dea3874b6c605e4cabccca0b7ef964f
Kanstantsin Kulak <kkulak24@gmail.com>
2020-08-09 19:28:40 +0300

    feat: add settings button

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { faSlidersH } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -8,6 +9,7 @@ import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 })
 export class HeaderComponent implements OnInit {
   faUserCircle = faUserCircle;
+  faSlidersH = faSlidersH;
 
   constructor() { }
 
