70c49f50f5d27f4723680b8d0a5ddea7e805e71d
Kanstantsin Kulak <kkulak24@gmail.com>
2020-08-17 17:09:44 +0300

    fix: fix return type

--- src/app/components/search-item/search-item.component.ts
+++ src/app/components/search-item/search-item.component.ts
@@ -18,22 +18,13 @@ export class SearchItemComponent implements OnInit {
   constructor() {
   }
 
-  public ngOnInit(): void {
-    let dateDiff = this.dateDiff();
-    if (dateDiff.diffDays > -7) {
-      this.borderClass = "public_date_seven_days";
-    } else {
-      this.borderClass = dateDiff.diffMonth > -1 ? "public_date_month" : (dateDiff.diffMonth < -6 ? "public_date_six_month" : "public_date_default");
-    }
-  }
-
-  private dateDiff(): { diffMins: number; diffDays: number; diffMs: number; diffMonth: number; diffHrs: number } {
+  private dateDiff(): { [key: string]: number } {
     let publicDate: Date = new Date(this.item.snippet.publishedAt);
-    let diffMs = (publicDate.valueOf() - new Date().valueOf());
-    let diffDays = Math.floor(diffMs / 86400000);
-    let diffHrs = Math.floor((diffMs % 86400000) / 3600000);
-    let diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000);
-    let diffMonth = diffDays / 31;
+    let diffMs: number = (publicDate.valueOf() - new Date().valueOf());
+    let diffDays: number = Math.floor(diffMs / 86400000);
+    let diffHrs: number = Math.floor((diffMs % 86400000) / 3600000);
+    let diffMins: number = Math.round(((diffMs % 86400000) % 3600000) / 60000);
+    let diffMonth: number = diffDays / 31;
     return {
       diffMs: diffMs,
       diffDays: diffDays,
@@ -41,6 +32,16 @@ export class SearchItemComponent implements OnInit {
       diffMins: diffMins,
       diffMonth: diffMonth
     };
+  }
+
+  public ngOnInit(): void {
+    let dateDiff: { [p: string]: number } = this.dateDiff();
+    if (dateDiff.diffDays > -7) {
+      this.borderClass = 'public_date_seven_days';
+    } else {
+      this.borderClass = dateDiff.diffMonth > -1 ? 'public_date_month' :
+        (dateDiff.diffMonth < -6 ? 'public_date_six_month' : 'public_date_default');
+    }
   }
 
 }
