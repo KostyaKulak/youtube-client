52920a689043fe544bb3dae62d3608957ea7c18b
Kanstantsin Kulak <kkulak24@gmail.com>
2020-08-17 18:12:10 +0300

    feat: add sorting

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {DataService} from '../../services/data.service';
+import {SortConfig} from '../../config/sort.config';
+import {SortType} from '../../config/sort.type';
 
 @Component({
   selector: 'app-filter',
@@ -9,10 +11,46 @@ import {DataService} from '../../services/data.service';
 export class FilterComponent implements OnInit {
   public hidden: boolean;
 
-  constructor(private data: DataService) {
+  constructor(private data: DataService, private sortConfig: SortConfig) {
   }
 
   public ngOnInit(): void {
     this.data.currentFilterHiddenState.subscribe(hidden => this.hidden = hidden);
+  }
+
+  public sortResults(sortType: SortType): void {
+    this.sortConfig.changeSortType(sortType);
+  }
+
+  public sortResultsDate(): void {
+    let currentSortType: SortType;
+    this.sortConfig.currentSortType.subscribe(sortType => currentSortType = sortType);
+    switch (currentSortType) {
+      case SortType.DATE_ASC:
+        this.sortResults(SortType.DATE_DEC);
+        break;
+      case SortType.DATE_DEC:
+        this.sortResults(SortType.DEFAULT);
+        break;
+      default:
+        this.sortResults(SortType.DATE_ASC);
+        break;
+    }
+  }
+
+  public sortResultsCountOfViews(): void {
+    let currentSortType: SortType;
+    this.sortConfig.currentSortType.subscribe(sortType => currentSortType = sortType);
+    switch (currentSortType) {
+      case SortType.COUNT_OF_VIEWS_ASC:
+        this.sortResults(SortType.COUNT_OF_VIEWS_DEC);
+        break;
+      case SortType.COUNT_OF_VIEWS_DEC:
+        this.sortResults(SortType.DEFAULT);
+        break;
+      default:
+        this.sortResults(SortType.COUNT_OF_VIEWS_ASC);
+        break;
+    }
   }
 }
--- src/app/components/search-results/search-results.component.ts
+++ src/app/components/search-results/search-results.component.ts
@@ -2,6 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {SearchResponse} from '../../models/search-response.model';
 import {HttpClient} from '@angular/common/http';
 import {DataService} from '../../services/data.service';
+import {SortType} from '../../config/sort.type';
+import {SortConfig} from '../../config/sort.config';
+import {SearchItem} from '../../models/search-item.model';
 
 @Component({
   selector: 'app-search-results',
@@ -13,12 +16,43 @@ export class SearchResultsComponent implements OnInit {
   public searchResponse: SearchResponse;
   public hidden: boolean;
 
-  constructor(private http: HttpClient, private data: DataService) {
+  constructor(private http: HttpClient, private data: DataService, private sortConfig: SortConfig) {
+  }
+
+  private sortResults(sortType: SortType): void {
+    this.searchResponse?.items.sort((a: SearchItem, b: SearchItem) => {
+      let compare: number;
+      switch (sortType) {
+        case SortType.DATE_ASC:
+          compare = new Date(a.snippet.publishedAt).getMilliseconds()
+            - new Date(b.snippet.publishedAt).getMilliseconds();
+          break;
+        case SortType.DATE_DEC:
+          compare = new Date(b.snippet.publishedAt).getMilliseconds()
+            - new Date(a.snippet.publishedAt).getMilliseconds();
+          break;
+        case SortType.COUNT_OF_VIEWS_ASC:
+          compare = Number(a.statistics.viewCount) - Number(b.statistics.viewCount);
+          break;
+        case SortType.COUNT_OF_VIEWS_DEC:
+          compare = Number(b.statistics.viewCount) - Number(a.statistics.viewCount);
+          break;
+        default:
+          compare = 0;
+          break;
+      }
+      return compare;
+    });
+    if (sortType === SortType.DEFAULT) {
+      this.http.get<SearchResponse>(this.url)
+        .subscribe((searchResponse: SearchResponse) => this.searchResponse = searchResponse);
+    }
   }
 
   public ngOnInit(): void {
     this.http.get<SearchResponse>(this.url)
       .subscribe((searchResponse: SearchResponse) => this.searchResponse = searchResponse);
     this.data.currentResultsHiddenState.subscribe(hidden => this.hidden = hidden);
+    this.sortConfig.currentSortType.subscribe(sortType => this.sortResults(sortType));
   }
 }
