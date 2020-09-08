c1e780051e87222d62cd064574b30f07efd6808e
Kanstantsin Kulak <kkulak24@gmail.com>
2020-09-08 03:03:36 +0300

    fix: fix filter display

--- src/app/youtube/components/search-item/search-item.component.ts
+++ src/app/youtube/components/search-item/search-item.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {SearchItem} from '../../models/search-item.model';
 import {faComments, faEye, faHeart, faHeartBroken, IconDefinition} from '@fortawesome/free-solid-svg-icons';
-import {findDateDiff} from '../../../shared/utils/date.utils';
+import {getBorderClass} from '../../../shared/utils/class.utils';
 
 @Component({
   selector: 'app-search-item',
@@ -20,13 +20,7 @@ export class SearchItemComponent implements OnInit {
   }
 
   public ngOnInit(): void {
-    let dateDiff: { [p: string]: number } = findDateDiff(this.item);
-    if (dateDiff.diffDays > -7) {
-      this.borderClass = 'public_date_seven_days';
-    } else {
-      this.borderClass = dateDiff.diffMonth > -1 ? 'public_date_month' :
-        (dateDiff.diffMonth < -6 ? 'public_date_six_month' : 'public_date_default');
-    }
+    this.borderClass = getBorderClass(this.item);
   }
 
 }
